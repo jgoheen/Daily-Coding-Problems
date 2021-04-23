781589c552ff422e531b4fc0b04722d6938ab287
jgoheen <joshgoheen@gmail.com>
2021-04-23 18:01:14 -0500

    Update exercise157-check_for_palindrome.py

--- exercise157-check_for_palindrome.py
+++ exercise157-check_for_palindrome.py
@@ -1,10 +1,7 @@
 '''
-
 Given a string, determine whether any permutation of it is a palindrome.
-
 For example, carrace should return true, since it can be rearranged to form racecar, 
 which is a palindrome. daily should return false, since there's no rearrangement that can form a palindrome.
-
 '''
 
 def palindromeCheck(string_check):
@@ -20,15 +17,16 @@ def palindromeCheck(string_check):
         for i in string_check:
             if string_check.count(i) % 2 == 0:
                 continue
-            elif string_check.count(i) % 2 == 1:
+            if string_check.count(i) % 2 == 1:
                 num_of_odd_chars += 1 
-        if num_of_odd_chars > 1:
+        if num_of_odd_chars % 2 == 0:
+            print(num_of_odd_chars)
             return "false"
         else:
             return "true"
 
 s1 = "carrac"
-s2 = "idecccide"
+s2 = "iiiiiuuuuhhjjkkmmnyny"
 
 print(palindromeCheck(s1))
 print(palindromeCheck(s2))
